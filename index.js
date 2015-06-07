120f88b464c5cce9f195b618b2d5dfaaae33997c
Harri Kovalainen <hakovala@gmail.com>
2015-06-07 16:31:45 +0300

    Change to more versatile require system
    Submodule require target can be filepath to module or gulpfile, and
    dependency path.
    
    Signed-off-by: Harri Kovalainen <hakovala@gmail.com>

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ module.exports = function (gulp, sep) {
 	sep = sep || ':';
 
 	var submodules = {};
-	gulp.submodule = function(submodule, filepath) {
-		filepath = filepath || submodule;
-		filepath = path.resolve(filepath);
 
-		// if filepath is directory, append 'gulpfile.js' to the path
-		if (fs.lstatSync(filepath).isDirectory()) {
-			filepath = path.join(filepath, 'gulpfile.js');
+	gulp.submodule = function(submodule, opt) {
+		var source;
+
+		if (opt.package || opt.pkg) {
+			source = opt.package || opt.pkg;
+		} else {
+			source = opt.filepath || submodule;
+			source = path.resolve(source);
+
+			// if source is directory, append 'gulpfile.js' to the path
+			if (fs.lstatSync(source).isDirectory()) {
+				source = path.join(source, 'gulpfile.js');
+			}
 		}
 
 		// get submodules task name
@@ -22,6 +29,7 @@ module.exports = function (gulp, sep) {
 		}
 
 		submodules[submodule] = { tasks: [], ret: null };
+
 		// cache original gulp.task method
 		var _task = gulp.task;
 		gulp.task = function(name, deps, fn) {
@@ -35,7 +43,7 @@ module.exports = function (gulp, sep) {
 			// call original gulp.task method
 			return _task.call(this, getName(name), deps, fn);
 		};
-		var mod = require(filepath);
+		var mod = require(source);
 		submodules[submodule].ret = mod;
 		// restore original gulp.task method
 		gulp.task = _task;
